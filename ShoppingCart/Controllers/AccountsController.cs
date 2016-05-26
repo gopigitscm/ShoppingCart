79c9d85326323df947f27022bfd475bb83cd2b56
gkatari <gkatari@csc.com>
2016-05-26 12:09:10 +0530

    dev2

--- ShoppingCart/Controllers/AccountsController.cs
+++ ShoppingCart/Controllers/AccountsController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using ShoppingCartApp.Models;
